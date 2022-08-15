fb2cfebc1a46696fb85dc04251650fdc25775f08
Danil <danila_monster@mail,ru>
2022-08-15 12:26:56 +0300

    первая задача c поправкой

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -7,13 +7,13 @@ int FindQuarter(int x, int y)
 {
     if (x > 0 && y > 0)
         return 1;
-    if (x < 0 && y > 0)
+    else if (x < 0 && y > 0)
         return 2;
-    if (x < 0 && y < 0)
+    else if (x < 0 && y < 0)
         return 3;
-    if (x > 0 && y < 0)
+    else if (x > 0 && y < 0)
         return 4; 
-
+    else
     return 0;               
 }
 
