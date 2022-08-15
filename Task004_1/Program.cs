d44a1d9074d6da39b88528a126c59289ac806cb1
Danil <danila_monster@mail,ru>
2022-08-15 16:39:53 +0300

    четвертая задача верс2

---
+++ Task004_1/Program.cs
@@ -0,0 +1,17 @@
+﻿void squareNumbers(int x)
+{
+    Console.Write($"{x} -> ");
+    
+    int index = 1;
+    while(index < x)
+    {
+        Console.Write($"{index * index}, ");
+        index++;
+    }
+    Console.Write($"{index * index}, ");
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+squareNumbers(num);
