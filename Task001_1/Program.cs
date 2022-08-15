32a1d77102c8bc7478adf7db4d383517e8c66ced
Danil <danila_monster@mail,ru>
2022-08-15 12:37:54 +0300

    первая задача версия3

--- Task001_1/Program.cs
+++ Task001_1/Program.cs
@@ -6,13 +6,17 @@
 void FindQuarter(int x, int y)
 {
     if (x > 0 && y > 0)
-        System.Console.WriteLine("1 четверть");
+        //System.Console.WriteLine("1 четверть");
+        System.Console.WriteLine($"Точка ({x};{y}) в 1 четверти");
     else if (x < 0 && y > 0)
-        System.Console.WriteLine("2 четверть");
+        //System.Console.WriteLine("2 четверть");
+        System.Console.WriteLine($"Точка ({x};{y}) во 2 четверти");
     else if (x < 0 && y < 0)
-        System.Console.WriteLine("3 четверть");
+        //System.Console.WriteLine("3 четверть");
+        System.Console.WriteLine($"Точка ({x};{y}) в 3 четверти");
     else if (x > 0 && y < 0)
-        System.Console.WriteLine("4 четверть"); 
+        //System.Console.WriteLine("4 четверть");
+        System.Console.WriteLine($"Точка ({x};{y}) в 4 четверти"); 
     else
         System.Console.WriteLine("Точка на координатной оси");               
 }
