54e70cd73dd5ad72ea5a486693ce25c92a8e9abb
Danil <danila_monster@mail,ru>
2022-08-15 12:25:21 +0300

    первая задача

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает 
+// на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости,
+//в которой находится эта точка.
+
+int FindQuarter(int x, int y)
+{
+    if (x > 0 && y > 0)
+        return 1;
+    if (x < 0 && y > 0)
+        return 2;
+    if (x < 0 && y < 0)
+        return 3;
+    if (x > 0 && y < 0)
+        return 4; 
+
+    return 0;               
+}
+
+System.Console.WriteLine(FindQuarter(4, 7));
+System.Console.WriteLine(FindQuarter(-19, 7));
+System.Console.WriteLine(FindQuarter(-1, -3));
+System.Console.WriteLine(FindQuarter(4, -8));
+System.Console.WriteLine(FindQuarter(4, 0));
