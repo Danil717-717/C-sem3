9cbe4ec76c680451aca683b4309abbc410cc5e16
Danil <danila_monster@mail,ru>
2022-08-15 17:51:01 +0300

    DZ003

---
+++ DZ_Task003/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 4, 9. 
+// 5 -> 1, 8, 27, 64, 125
+// 3 -> 1, 8, 27 
+// 5 -> 1, 8, 27, 64, 125
+
+void squareNumbers(int x)
+{
+    Console.Write($"{x} -> ");
+    
+    int index = 1;
+    while(index < x)
+    {
+        Console.Write($"{index * index * index}, ");
+        index++;
+    }
+    Console.Write($"{index * index * index}, ");
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+squareNumbers(num);
