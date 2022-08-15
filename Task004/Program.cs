dd4b9916bf5b2a0d0862fcf4c94a88d444123130
Danil <danila_monster@mail,ru>
2022-08-15 16:23:53 +0300

    четвертая задача

---
+++ Task004/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+//  и выдаёт таблицу квадратов чисел от 1 до N.
+
+void squareNumber(int x)
+{
+    Console.Write($"{x} -> ");
+
+    int i = 1;
+    while(i < x)
+    {
+        Console.Write($"{i * i},  " );
+        i++;
+    }
+
+    Console.WriteLine($"{x * x}, ");    
+}
+
+squareNumber(5);
+squareNumber(2);
+squareNumber(6);
+squareNumber(9);
