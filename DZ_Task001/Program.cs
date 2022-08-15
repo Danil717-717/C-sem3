1fd387590a3b327c5ade9f5aaf97749c88b3c76c
Danil <danila_monster@mail,ru>
2022-08-15 17:37:07 +0300

    DZ001

--- DZ_Task001/Program.cs
+++ DZ_Task001/Program.cs
@@ -1,4 +1,4 @@
-﻿// : Напишите программу, которая принимает на вход пятизначное 
+﻿//  Напишите программу, которая принимает на вход пятизначное 
 // число и проверяет, является ли оно палиндромом.
 
 void palindrom()
@@ -6,23 +6,26 @@ void palindrom()
         //int a = new Random().Next(10000, 100000);
         Console.Write("Введите 5значное число: ");
         int a = Convert.ToInt32(Console.ReadLine()); 
-        int num1 = a / 10000 % 10;
-        int num2 = a / 1000 % 10;
-        int rev1 = a / 10 % 10;
-        int rev2 = a % 10;
-        int nul = a / 10000;
+        int x1 = a / 10000 % 10;
+        int x2 = a / 1000 % 10;
+        int y1 = a / 10 % 10;
+        int y2 = a % 10;
+        int n5 = a / 10000;
         
-            if (nul < 1 || nul > 9){
+            if (n5 < 1 || n5 > 9)
+            {
             Console.WriteLine(a + " -> " + "Не 5значное число");
             }
-            else if  (num1 == rev2 && num2 == rev1){
+            else if  (x1 == y2 && x2 == y1)
+            {
             Console.WriteLine(a + " -> Да, это палиндром");
             }
-            else{
-            Console.WriteLine(a + " -> нет, это не палиндром");
+            else
+            {
+            Console.WriteLine(a + " -> Нет, это не палиндром");
             }
         }
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 5; i++)
         { 
             palindrom();
     }
